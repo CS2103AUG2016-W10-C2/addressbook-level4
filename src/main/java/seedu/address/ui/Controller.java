3acc3e287096442c52137faeb1f639b462fcd143
Karen Ang <thekarenang@gmail.com>
2016-10-23 19:11:43 +0800

    Add controller abstract class

---
+++ src/main/java/seedu/address/ui/Controller.java
@@ -0,0 +1,9 @@
+package seedu.address.ui;
+
+/**
+ * Abstract class for all app view controllers.
+ * Forces controllers to implement an init method.
+ */
+public abstract class Controller {
+    abstract void init();
+}
