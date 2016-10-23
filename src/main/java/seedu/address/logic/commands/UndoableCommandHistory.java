f006c5adc3b84906f956df956b565f2d858941a3
VincentSeng <vin9492@hotmail.com>
2016-10-23 13:47:57 +0800

    Add UndoableCommandHistory for tracking UndoableCommands

---
+++ src/main/java/seedu/address/logic/commands/UndoableCommandHistory.java
@@ -0,0 +1,23 @@
+package seedu.address.logic.commands;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * Stack of successfully executed undoable commands.
+ */
+public class UndoableCommandHistory {
+    Deque<UndoableCommand> commandInternalQueue = new ArrayDeque<UndoableCommand>();
+    public static class UndoableCommandNotFoundException extends Exception {};
+    
+    public void push(UndoableCommand undoableCommand) {
+        commandInternalQueue.addFirst(undoableCommand);
+    }
+    
+    public UndoableCommand getMostRecentUndoableCommand() throws UndoableCommandNotFoundException {
+        if (commandInternalQueue.isEmpty()){
+            throw new UndoableCommandNotFoundException();
+        }
+        return commandInternalQueue.poll();
+    }
+}
