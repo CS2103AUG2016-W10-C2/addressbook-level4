8f7d318f961ad6a11d411557ec05ca9c24915f82
VincentSeng <vin9492@hotmail.com>
2016-10-23 13:48:28 +0800

    Add undo command

---
+++ src/main/java/seedu/address/logic/commands/UndoCommand.java
@@ -0,0 +1,31 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.commands.UndoableCommandHistory.UndoableCommandNotFoundException;
+import seedu.address.model.Model;
+
+/**
+ * Undo the previous undoable command.
+ */
+public class UndoCommand extends Command {
+
+    public static final String COMMAND_WORD = "undo";
+    public static final String MESSAGE_FAILURE = "No undoable commands found!";
+    private UndoableCommandHistory undoableCommandHistory;
+
+    public UndoCommand() {}
+    
+    public void setData(Model model, UndoableCommandHistory undoableCommandQueue) {
+        this.model = model;
+        this.undoableCommandHistory = undoableCommandQueue;
+    }
+    
+    @Override
+    public CommandResult execute() {
+        try {
+            UndoableCommand undoableCommand = undoableCommandHistory.getMostRecentUndoableCommand();
+            return undoableCommand.unexecute();
+        } catch (UndoableCommandNotFoundException e) {
+            return new CommandResult(MESSAGE_FAILURE);
+        }
+    }
+}
