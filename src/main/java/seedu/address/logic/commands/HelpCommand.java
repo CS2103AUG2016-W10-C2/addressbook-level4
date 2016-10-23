ead0adeefc95a951fe51486e536c5cc35453bbfb
Karen Ang <thekarenang@gmail.com>
2016-10-24 00:09:04 +0800

    Update events and command for help

--- src/main/java/seedu/address/logic/commands/HelpCommand.java
+++ src/main/java/seedu/address/logic/commands/HelpCommand.java
@@ -2,7 +2,7 @@ package seedu.address.logic.commands;
 
 
 import seedu.address.commons.core.EventsCenter;
-import seedu.address.commons.events.ui.ShowHelpRequestEvent;
+import seedu.address.commons.events.ui.ShowHelpListEvent;
 
 /**
  * Format full help instructions for every command for display.
@@ -14,13 +14,13 @@ public class HelpCommand extends Command {
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows program usage instructions.\n"
             + "Example: " + COMMAND_WORD;
 
-    public static final String SHOWING_HELP_MESSAGE = "Opened help window.";
+    public static final String SHOWING_HELP_MESSAGE = "";
 
     public HelpCommand() {}
 
     @Override
     public CommandResult execute() {
-        EventsCenter.getInstance().post(new ShowHelpRequestEvent());
+        EventsCenter.getInstance().post(new ShowHelpListEvent());
         return new CommandResult(SHOWING_HELP_MESSAGE);
     }
 }
