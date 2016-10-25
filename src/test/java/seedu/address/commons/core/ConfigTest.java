2988e2d1d5c9db1f3f29e3a5cf5db05238e1f4d5
Tjong Anthony <tjonganthony@gmail.com>
2016-10-26 02:46:20 +0800

    fix more graddle error

--- src/test/java/seedu/address/commons/core/ConfigTest.java
+++ src/test/java/seedu/address/commons/core/ConfigTest.java
@@ -17,7 +17,7 @@ public class ConfigTest {
         String defaultConfigAsString = "App title : PriorityQ\n" +
                 "Current log level : INFO\n" +
                 "Preference file Location : preferences.json\n" +
-                "Local data file location : data/addressbook.xml\n" +
+                "Local data file location : data/PriorityQ.xml\n" +
                 "TaskManager name : TypicalTaskManagerName";
 
         assertEquals(defaultConfigAsString, new Config().toString());
