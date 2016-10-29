9f6fca7ad19a8c944a4aee8084410f685d9a8a7a
Karen Ang <thekarenang@gmail.com>
2016-10-30 01:10:24 +0800

    Faster gui tests... 😴

--- src/test/java/guitests/guihandles/GuiHandle.java
+++ src/test/java/guitests/guihandles/GuiHandle.java
@@ -62,11 +62,11 @@ public class GuiHandle {
     }
 
     public void pressEnter() {
-        guiRobot.type(KeyCode.ENTER).sleep(500);
+        guiRobot.type(KeyCode.ENTER).sleep(200);
     }
 
     public void pressEscape() {
-        guiRobot.type(KeyCode.ESCAPE).sleep(500);
+        guiRobot.type(KeyCode.ESCAPE).sleep(200);
     }
 
     protected String getTextFromLabel(String fieldId, Node parentNode) {
