6db6100af765db5922359a93c305de9f2bee6d47
Karen Ang <thekarenang@gmail.com>
2016-10-29 20:09:03 +0800

    Update helpers for styling

--- src/main/java/seedu/address/ui/util/GuiUtil.java
+++ src/main/java/seedu/address/ui/util/GuiUtil.java
@@ -20,25 +20,44 @@ public class GuiUtil {
 
     public static final String EVENT_DATE_SEPARATOR = " - ";
 
+    public static final String EVENT_DESCRIPTION_STYLE_CLASS = "event";
+
     private static String PAST_STYLE_CLASS = "past";
     private static String ACTIVE_STYLE_CLASS = "present";
+    private static String OVERDUE_STYLE_CLASS = "overdue";
 
     public static ChangeListener<Boolean> getCheckBoxEventListener(int idx) {
         return (ov, old_val, new_val) -> EventsCenter.getInstance().post(new MarkTaskEvent(idx, new_val));
     }
 
-    public static String getDeadlineStyling(LocalDateTime dateTime) {
+    public static  String getTaskStyling(boolean isMarked) {
+        return getDeadlineStyling(isMarked, null);
+    }
+
+    public static String getDeadlineStyling(boolean isMarked, LocalDateTime deadline) {
+        if (isMarked) {
+            return PAST_STYLE_CLASS;
+        }
+
+        if (deadline == null) {
+            return "";
+        }
+
         LocalDateTime now = LocalDateTime.now();
         LocalDateTime midnight = LocalDateTime.of(now.toLocalDate().plusDays(1), LocalTime.MIDNIGHT);
-        if (dateTime.isBefore(now)) {
-            return PAST_STYLE_CLASS;
-        } else if (dateTime.isAfter(now) && dateTime.isBefore(midnight)) {
+
+        if (deadline.isBefore(now)) {
+            return OVERDUE_STYLE_CLASS;
+        }
+
+        if (deadline.isAfter(now) && deadline.isBefore(midnight)) {
             return ACTIVE_STYLE_CLASS;
         }
         return "";
     }
 
     public static String getEventStyling(LocalDateTime startTime, LocalDateTime endTime) {
+        assert (startTime != null && endTime != null);
         LocalDateTime now = LocalDateTime.now();
         if (endTime.isBefore(now)) {
             return PAST_STYLE_CLASS;
