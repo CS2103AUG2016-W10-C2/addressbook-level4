54986da16e22aaf310d8e31e7d814ee3bb958e6b
Tjong Anthony <tjonganthony@gmail.com>
2016-11-08 01:53:49 +0800

    add save command test

---
+++ src/test/java/seedu/priorityq/logic/commands/SaveCommandTest.java
@@ -0,0 +1,39 @@
+package seedu.priorityq.logic.commands;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.nio.file.InvalidPathException;
+
+import org.junit.Test;
+
+import seedu.priorityq.commons.exceptions.IllegalValueException;
+
+
+//@@author A0126539Y
+public class SaveCommandTest {
+
+    @Test(expected = IllegalValueException.class)
+    public void nullLocation() throws Exception {
+        new SaveCommand(null);
+    }
+    
+    @Test(expected = IllegalValueException.class)
+    public void emptyStringLocation() throws Exception {
+        new SaveCommand("");
+    }
+    
+    @Test(expected = InvalidPathException.class)
+    public void invalidLocation() throws Exception{
+        new SaveCommand("invalid.json");
+    }
+    
+    @Test
+    public void saveCommand() {
+        try {
+            
+        } catch (Exception e) {
+            fail();
+        }
+    }
+}
